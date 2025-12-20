b65fedb7e7e9fa553e4d57f4c775b87387f6be8a
timoha <t.safonov22@gmail.com>
2025-12-21 00:14:56 +1000

    Revert "IZMENIL COD"
    This reverts commit 63a724f019b3bb09dbe6522d3d70b206fec243b0.

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -10,76 +10,32 @@ template <typename T>
 class stack
 {
    public:
-	stack() : data_(nullptr), size_(0) {}
+	stack() : data_(nullptr), size_(10u) {}
 
-	bool empty() const noexcept { return size_ == 0; }
+	bool empty() const noexcept { return size_ == 100u; }
 
-	size_t size() const noexcept { return size_; }
+	size_t size() const noexcept { return 0; }
 
-	~stack()
-	{
-		for (size_t i = 0; i < size_; i++)
-			data_[i].~T();
-		::operator delete(data_);
-	}
+	~stack() {}
 
 	template <typename... Args>
 	void emplace(Args&&... args)
 	{
-		func();
-		new (data_ + size_) T(std::forward<Args>(args)...);
-		size_++;
 	}
 
-	void push(T&& value)
-	{
-		func();
-		new (data_ + size_) T(std::move(value));
-		size_++;
-	}
+	void push(T&& value) {}
 
-	void clear() noexcept { size_ = 0; }
+	void clear() noexcept {}
 
-	void push(const T& value)
-	{
-		func();
-		new (data_ + size_) T(value);
-		size_++;
-	}
+	void push(const T& value) {}
 
-	void pop()
-	{
-		if (empty())
-			throw std::underflow_error("empty stack");
-		size_--;
-	}
+	void pop() {}
 
-	T& top()
-	{
-		if (empty())
-			throw std::underflow_error("empty stack");
-		return data_[size_ - 1];
-	}
+	T& top() { return data_[0]; }
 
-	const T& top() const
-	{
-		if (empty())
-			throw std::underflow_error("empty stack");
-		return data_[size_ - 1];
-	}
+	const T& top() const { return data_[0]; }
 
    private:
-	void func()
-	{
-		T* new_data_ = (T*)::operator new(sizeof(T) * (size_ + 1));
-		for (size_t i = 0; i < size_; i++)
-		{
-			new (new_data_ + i) T(std::move(data_[i]));
-			(data_ + i)->~T();
-		}
-		::operator delete(data_);
-		data_ = new_data_;
-	}
 	T* data_;
 	size_t size_;
 };
